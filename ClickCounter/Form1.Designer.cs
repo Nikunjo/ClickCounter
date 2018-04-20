fb396f82a12b7a33427ffe9a8511a304d221d396
Nikunjo <nikonjo@live.com>
2018-04-20 15:27:03 +0600

    first commit

---
+++ ClickCounter/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace ClickCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bt1 = new System.Windows.Forms.Button();
+            this.score = new System.Windows.Forms.Label();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.bt3 = new System.Windows.Forms.Button();
+            this.bt4 = new System.Windows.Forms.Button();
+            this.bt5 = new System.Windows.Forms.Button();
+            this.bt7 = new System.Windows.Forms.Button();
+            this.bt6 = new System.Windows.Forms.Button();
+            this.bt8 = new System.Windows.Forms.Button();
+            this.bt9 = new System.Windows.Forms.Button();
+            this.bt10 = new System.Windows.Forms.Button();
+            this.playagain = new System.Windows.Forms.Button();
+            this.ins = new System.Windows.Forms.Label();
+            this.exit = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.scr = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bt1
+            // 
+            resources.ApplyResources(this.bt1, "bt1");
+            this.bt1.Name = "bt1";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // score
+            // 
+            resources.ApplyResources(this.score, "score");
+            this.score.Name = "score";
+            // 
+            // bt2
+            // 
+            resources.ApplyResources(this.bt2, "bt2");
+            this.bt2.Name = "bt2";
+            this.bt2.UseVisualStyleBackColor = true;
+            this.bt2.Click += new System.EventHandler(this.bt2_Click);
+            // 
+            // bt3
+            // 
+            resources.ApplyResources(this.bt3, "bt3");
+            this.bt3.Name = "bt3";
+            this.bt3.UseVisualStyleBackColor = true;
+            this.bt3.Click += new System.EventHandler(this.bt3_Click);
+            // 
+            // bt4
+            // 
+            resources.ApplyResources(this.bt4, "bt4");
+            this.bt4.Name = "bt4";
+            this.bt4.UseVisualStyleBackColor = true;
+            this.bt4.Click += new System.EventHandler(this.bt4_Click);
+            // 
+            // bt5
+            // 
+            resources.ApplyResources(this.bt5, "bt5");
+            this.bt5.Name = "bt5";
+            this.bt5.UseVisualStyleBackColor = true;
+            this.bt5.Click += new System.EventHandler(this.bt5_Click);
+            // 
+            // bt7
+            // 
+            resources.ApplyResources(this.bt7, "bt7");
+            this.bt7.Name = "bt7";
+            this.bt7.UseVisualStyleBackColor = true;
+            this.bt7.Click += new System.EventHandler(this.bt7_Click);
+            // 
+            // bt6
+            // 
+            resources.ApplyResources(this.bt6, "bt6");
+            this.bt6.Name = "bt6";
+            this.bt6.UseVisualStyleBackColor = true;
+            this.bt6.Click += new System.EventHandler(this.bt6_Click);
+            // 
+            // bt8
+            // 
+            resources.ApplyResources(this.bt8, "bt8");
+            this.bt8.Name = "bt8";
+            this.bt8.UseVisualStyleBackColor = true;
+            this.bt8.Click += new System.EventHandler(this.bt8_Click);
+            // 
+            // bt9
+            // 
+            resources.ApplyResources(this.bt9, "bt9");
+            this.bt9.Name = "bt9";
+            this.bt9.UseVisualStyleBackColor = true;
+            this.bt9.Click += new System.EventHandler(this.bt9_Click);
+            // 
+            // bt10
+            // 
+            resources.ApplyResources(this.bt10, "bt10");
+            this.bt10.Name = "bt10";
+            this.bt10.UseVisualStyleBackColor = true;
+            this.bt10.Click += new System.EventHandler(this.bt10_Click);
+            // 
+            // playagain
+            // 
+            resources.ApplyResources(this.playagain, "playagain");
+            this.playagain.Name = "playagain";
+            this.playagain.UseVisualStyleBackColor = true;
+            this.playagain.Click += new System.EventHandler(this.playagain_Click);
+            // 
+            // ins
+            // 
+            resources.ApplyResources(this.ins, "ins");
+            this.ins.Name = "ins";
+            // 
+            // exit
+            // 
+            resources.ApplyResources(this.exit, "exit");
+            this.exit.Name = "exit";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 800;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // scr
+            // 
+            resources.ApplyResources(this.scr, "scr");
+            this.scr.Name = "scr";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Name = "label2";
+            // 
+            // pb2
+            // 
+            resources.ApplyResources(this.pb2, "pb2");
+            this.pb2.Image = global::ClickCounter.Properties.Resources.Delete_512px;
+            this.pb2.Name = "pb2";
+            this.pb2.TabStop = false;
+            this.pb2.Click += new System.EventHandler(this.pb2_Click);
+            // 
+            // pb1
+            // 
+            resources.ApplyResources(this.pb1, "pb1");
+            this.pb1.Image = global::ClickCounter.Properties.Resources.Subtract_512px;
+            this.pb1.Name = "pb1";
+            this.pb1.TabStop = false;
+            this.pb1.Click += new System.EventHandler(this.pb1_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.scr);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ins);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.playagain);
+            this.Controls.Add(this.bt10);
+            this.Controls.Add(this.bt8);
+            this.Controls.Add(this.bt4);
+            this.Controls.Add(this.bt6);
+            this.Controls.Add(this.bt2);
+            this.Controls.Add(this.bt9);
+            this.Controls.Add(this.bt7);
+            this.Controls.Add(this.bt5);
+            this.Controls.Add(this.bt3);
+            this.Controls.Add(this.bt1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.Button bt5;
+        private System.Windows.Forms.Button bt7;
+        private System.Windows.Forms.Button bt6;
+        private System.Windows.Forms.Button bt8;
+        private System.Windows.Forms.Button bt9;
+        private System.Windows.Forms.Button bt10;
+        private System.Windows.Forms.Button playagain;
+        private System.Windows.Forms.Label ins;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label scr;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ClickCounter/Form1.cs
@@ -0,0 +1,529 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClickCounter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+       
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+          
+
+
+            
+                int x;
+                Random r = new Random();
+                x = r.Next(11);
+
+                if (x == 1)
+                {
+                    this.bt1.BackColor = Color.Green;
+                    this.bt2.BackColor = Color.Red;
+                    this.bt3.BackColor = Color.Red;
+                    this.bt4.BackColor = Color.Red;
+                    this.bt5.BackColor = Color.Red;
+                    this.bt6.BackColor = Color.Red;
+                    this.bt7.BackColor = Color.Red;
+                    this.bt8.BackColor = Color.Red;
+                    this.bt9.BackColor = Color.Red;
+                    this.bt10.BackColor = Color.Red;
+                }
+
+                else if (x == 2)
+                {
+                    this.bt1.BackColor = Color.Red;
+                    this.bt2.BackColor = Color.Green;
+                    this.bt3.BackColor = Color.Red;
+                    this.bt4.BackColor = Color.Red;
+                    this.bt5.BackColor = Color.Red;
+                    this.bt6.BackColor = Color.Red;
+                    this.bt7.BackColor = Color.Red;
+                    this.bt8.BackColor = Color.Red;
+                    this.bt9.BackColor = Color.Red;
+                    this.bt10.BackColor = Color.Red;
+                }
+                //
+
+               else if (x == 3)
+                {
+                    this.bt1.BackColor = Color.Red;
+                    this.bt2.BackColor = Color.Red;
+                    this.bt3.BackColor = Color.Green;
+                    this.bt4.BackColor = Color.Red;
+                    this.bt5.BackColor = Color.Red;
+                    this.bt6.BackColor = Color.Red;
+                    this.bt7.BackColor = Color.Red;
+                    this.bt8.BackColor = Color.Red;
+                    this.bt9.BackColor = Color.Red;
+                    this.bt10.BackColor = Color.Red;
+                }
+
+
+                //
+                else if (x == 4)
+                {
+                    this.bt1.BackColor = Color.Red;
+                    this.bt2.BackColor = Color.Red;
+                    this.bt3.BackColor = Color.Red;
+                    this.bt4.BackColor = Color.Green;
+                    this.bt5.BackColor = Color.Red;
+                    this.bt6.BackColor = Color.Red;
+                    this.bt7.BackColor = Color.Red;
+                    this.bt8.BackColor = Color.Red;
+                    this.bt9.BackColor = Color.Red;
+                    this.bt10.BackColor = Color.Red;
+                }
+
+                //
+
+                else if (x == 5)
+                {
+                    this.bt1.BackColor = Color.Red;
+                    this.bt2.BackColor = Color.Red;
+                    this.bt3.BackColor = Color.Red;
+                    this.bt4.BackColor = Color.Red;
+                    this.bt5.BackColor = Color.Green;
+                    this.bt6.BackColor = Color.Red;
+                    this.bt7.BackColor = Color.Red;
+                    this.bt8.BackColor = Color.Red;
+                    this.bt9.BackColor = Color.Red;
+                    this.bt10.BackColor = Color.Red;
+                }
+
+            //
+
+                else if (x == 6)
+                {
+                    this.bt1.BackColor = Color.Red;
+                    this.bt2.BackColor = Color.Red;
+                    this.bt3.BackColor = Color.Red;
+                    this.bt4.BackColor = Color.Red;
+                    this.bt5.BackColor = Color.Red;
+                    this.bt6.BackColor = Color.Green;
+                    this.bt7.BackColor = Color.Red;
+                    this.bt8.BackColor = Color.Red;
+                    this.bt9.BackColor = Color.Red;
+                    this.bt10.BackColor = Color.Red;
+                }
+
+
+            //
+
+                else if (x == 7)
+                {
+                    this.bt1.BackColor = Color.Red;
+                    this.bt2.BackColor = Color.Red;
+                    this.bt3.BackColor = Color.Red;
+                    this.bt4.BackColor = Color.Red;
+                    this.bt5.BackColor = Color.Red;
+                    this.bt6.BackColor = Color.Red;
+                    this.bt7.BackColor = Color.Green;
+                    this.bt8.BackColor = Color.Red;
+                    this.bt9.BackColor = Color.Red;
+                    this.bt10.BackColor = Color.Red;
+                }
+
+            ///
+
+                else if (x == 8)
+                {
+                    this.bt1.BackColor = Color.Red;
+                    this.bt2.BackColor = Color.Red;
+                    this.bt3.BackColor = Color.Red;
+                    this.bt4.BackColor = Color.Red;
+                    this.bt5.BackColor = Color.Red;
+                    this.bt6.BackColor = Color.Red;
+                    this.bt7.BackColor = Color.Red;
+                    this.bt8.BackColor = Color.Green;
+                    this.bt9.BackColor = Color.Red;
+                    this.bt10.BackColor = Color.Red;
+
+                   
+                }
+
+
+            //
+
+                else if (x == 9)
+                {
+                    this.bt1.BackColor = Color.Red;
+                    this.bt2.BackColor = Color.Red;
+                    this.bt3.BackColor = Color.Red;
+                    this.bt4.BackColor = Color.Red;
+                    this.bt5.BackColor = Color.Red;
+                    this.bt6.BackColor = Color.Red;
+                    this.bt7.BackColor = Color.Red;
+                    this.bt8.BackColor = Color.Red;
+                    this.bt9.BackColor = Color.Green;
+                    this.bt10.BackColor = Color.Red;
+                }
+
+
+
+            ///
+
+                else if (x == 10)
+                {
+                    this.bt1.BackColor = Color.Red;
+                    this.bt2.BackColor = Color.Red;
+                    this.bt3.BackColor = Color.Red;
+                    this.bt4.BackColor = Color.Red;
+                    this.bt5.BackColor = Color.Red;
+                    this.bt6.BackColor = Color.Red;
+                    this.bt7.BackColor = Color.Red;
+                    this.bt8.BackColor = Color.Red;
+                    this.bt9.BackColor = Color.Red;
+                    this.bt10.BackColor = Color.Green;
+                }
+
+             
+
+
+
+            }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        int count;
+
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            if (this.bt1.BackColor == Color.Red)
+            {
+                count -= 10;
+                scr.Text = count.ToString();
+               
+            }
+
+            if (this.bt1.BackColor == Color.Green)
+            {
+                count += 10;
+                scr.Text = count.ToString();
+               
+
+            }
+
+           
+
+        }
+
+        private void bt2_Click(object sender, EventArgs e)
+        {
+
+            if (this.bt2.BackColor == Color.Red)
+            {
+                count -= 10;
+                scr.Text = count.ToString();
+
+            }
+
+            if (this.bt2.BackColor == Color.Green)
+            {
+                count += 10;
+                scr.Text = count.ToString();
+
+
+            }
+
+        }
+
+        private void bt3_Click(object sender, EventArgs e)
+        {
+
+            if (this.bt3.BackColor == Color.Red)
+            {
+                count -= 10;
+                scr.Text = count.ToString();
+
+            }
+
+            if (this.bt3.BackColor == Color.Green)
+            {
+                count += 10;
+                scr.Text = count.ToString();
+
+
+            }
+
+        }
+
+        private void bt4_Click(object sender, EventArgs e)
+        {
+
+            if (this.bt4.BackColor == Color.Red)
+            {
+                count -= 10;
+                scr.Text = count.ToString();
+
+            }
+
+            if (this.bt4.BackColor == Color.Green)
+            {
+                count += 10;
+                scr.Text = count.ToString();
+
+
+            }
+
+        }
+
+        private void bt5_Click(object sender, EventArgs e)
+        {
+
+            if (this.bt5.BackColor == Color.Red)
+            {
+                count -= 10;
+                scr.Text = count.ToString();
+
+            }
+
+            if (this.bt5.BackColor == Color.Green)
+            {
+                count += 10;
+                scr.Text = count.ToString();
+
+
+            }
+
+        }
+
+        private void bt6_Click(object sender, EventArgs e)
+        {
+
+            if (this.bt6.BackColor == Color.Red)
+            {
+                count -= 10;
+                scr.Text = count.ToString();
+
+            }
+
+            if (this.bt6.BackColor == Color.Green)
+            {
+                count += 10;
+                scr.Text = count.ToString();
+
+
+            }
+
+        }
+
+        private void bt7_Click(object sender, EventArgs e)
+        {
+
+            if (this.bt7.BackColor == Color.Red)
+            {
+                count -= 10;
+                scr.Text = count.ToString();
+
+            }
+
+            if (this.bt7.BackColor == Color.Green)
+            {
+                count += 10;
+                scr.Text = count.ToString();
+
+
+            }
+
+        }
+
+        private void bt8_Click(object sender, EventArgs e)
+        {
+
+            if (this.bt8.BackColor == Color.Red)
+            {
+                count -= 10;
+                scr.Text = count.ToString();
+
+            }
+
+            if (this.bt8.BackColor == Color.Green)
+            {
+                count += 10;
+                scr.Text = count.ToString();
+
+
+            }
+
+        }
+
+        private void bt9_Click(object sender, EventArgs e)
+        {
+
+            if (this.bt9.BackColor == Color.Red)
+            {
+                count -= 10;
+                scr.Text = count.ToString();
+
+            }
+
+            if (this.bt9.BackColor == Color.Green)
+            {
+                count += 10;
+                scr.Text = count.ToString();
+
+
+            }
+
+        }
+
+        private void bt10_Click(object sender, EventArgs e)
+        {
+
+            if (this.bt10.BackColor == Color.Red)
+            {
+                count -= 10;
+                scr.Text = count.ToString();
+
+            }
+
+            if (this.bt10.BackColor == Color.Green)
+            {
+                count += 10;
+                scr.Text = count.ToString();
+
+
+            }
+
+        }
+
+        private void playagain_Click(object sender, EventArgs e)
+        {
+            if (playagain.Text == "Start")
+            {
+                count = 0;
+                scr.Text = count.ToString();
+
+
+                scr.Text = 0 + "";
+                playagain.Text = "Stop";
+                timer1.Enabled = true;
+                
+
+                bt1.Visible = true;
+                bt2.Visible = true;
+                bt3.Visible = true;
+                bt4.Visible = true;
+                bt5.Visible = true;
+                bt6.Visible = true;
+                bt7.Visible = true;
+                bt8.Visible = true;
+                bt9.Visible = true;
+                bt10.Visible = true;
+
+
+            }
+
+            else
+
+
+            {
+
+                playagain.Text = "Start";
+                timer1.Enabled = false;
+                bt1.Visible = false;
+                bt2.Visible = false;
+                bt3.Visible = false;
+                bt4.Visible = false;
+                bt5.Visible = false;
+                bt6.Visible = false;
+                bt7.Visible = false;
+                bt8.Visible = false;
+                bt9.Visible = false;
+                bt10.Visible = false;
+
+               
+              
+
+
+                
+            
+            }
+
+        }
+
+        private void pb1_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState != FormWindowState.Minimized)
+            {
+
+                this.WindowState = FormWindowState.Minimized;
+
+            }
+
+        }
+
+        private void pb2_Click(object sender, EventArgs e)
+        {
+
+            this.Close();
+        }
+
+
+        int Togmove;
+        int MVX;
+        int MVY;
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            Togmove = 1;
+            MVX = e.X;
+            MVY = e.Y;
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+            Togmove = 0;
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (Togmove == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - MVX, MousePosition.Y - MVY);
+            }
+
+        }
+
+
+       
+      
+               
+
+
+
+            }
+
+
+
+
+
+
+
+        }
+    
